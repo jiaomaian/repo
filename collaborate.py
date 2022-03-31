78c2b16713f8f4fd9482df8482c548a37e110786
jiaomaian <sapphire1998@vip.sina.com>
2022-03-31 16:20:00 -0500

    Create collaborate.py

---
+++ collaborate.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+<<<<<<< HEAD
+print("hello_world");
+print("hi :)");
+
+
+
+=======
+print("hi :)")
+>>>>>>> c6992796ab4f713a9fe01300f29c79750b8cd4c7
