963ddc64507de9d61c844597aba77bf884cce849
jiaomaian <sapphire1998@vip.sina.com>
2022-03-31 16:21:53 -0500

    Update collaborate.py

--- collaborate.py
+++ collaborate.py
@@ -5,12 +5,5 @@ Spyder Editor
 This is a temporary script file.
 """
 
-<<<<<<< HEAD
 print("hello_world");
 print("hi :)");
-
-
-
-=======
-print("hi :)")
->>>>>>> c6992796ab4f713a9fe01300f29c79750b8cd4c7
